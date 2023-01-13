9a6b3f23944e133e0e8c1197544bc804b7319a0c
Olga Felczak <ofelczak@gmail.com>
2023-01-13 16:56:03 +0100

    Added pseudocode and first question of quiz

---
+++ assets/js/logic.js
@@ -0,0 +1,64 @@
+// A start button function
+    // loads Q&A
+for (let i = 0; i < myQuestions.length; i++) {
+  console.log(myQuestions[i].question);
+  console.log(myQuestions[i].answers);
+}
+    // starts timer
+
+// timer starts
+    // Timer function
+    // countdown
+    // display
+    // get game stop
+    // stop at zero
+
+//
+
+// Timer connected to:
+    // Game start f
+    // Game stop f
+    // timer display
+    // Incorrect answer - time deduction
+
+// gamestart function
+    // trigger from button
+    // start timer
+    // call show questions and choices function
+    // hide submit button
+    // hide instructions
+
+// toggle class attribute hide
+    // start-screen
+    // questions container
+
+// Gamestop function
+    // trigger from
+        // last question
+        // timer zero
+    // announces score
+    // gets initials
+    // post to local storage
+    // show submit button
+    // show instructions
+
+// highscore function
+    // on load, update from local storage
+    // rank by score
+    // activate clear highscores button
+
+// clear highscore function
+    // clears LS
+// Q&A function
+    // get q&A
+    // display
+    // interactive
+    // on click
+        // show choice
+        // show correct answer
+        // clear
+        // get new questions
+
+// choice display
+
+// evaluate choice function
---
+++ assets/js/questions.js
@@ -0,0 +1,6 @@
+var myQuestions = [
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    answers: ["<script>", "<javascript>", "<js>"],
+  },
+];
