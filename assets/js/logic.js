a39406c291cedb004d23656306f6e4a1cbaf6c10
Olga Felczak <ofelczak@gmail.com>
2023-01-17 18:59:50 +0100

    Added questions and functions rendering them

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,63 +1,118 @@
 // A start button function
-    // loads Q&A
-for (let i = 0; i < myQuestions.length; i++) {
-  console.log(myQuestions[i].question);
-  console.log(myQuestions[i].answers);
+const buttonStart = document.getElementById("start");
+
+buttonStart.addEventListener("click", onButtonStartClick);
+
+function onButtonStartClick(e) {
+  let questions = document.getElementById("questions");
+  questions.classList.remove("hide");
+
+  let startScreen = document.getElementById("start-screen");
+  startScreen.classList.add("hide");
+
+  renderQuestion(myQuestions[0].question, myQuestions[0].answers);
+
+  countdown();
 }
-    // starts timer
+
+function renderQuestion(title, choices) {
+  renderQuestionTitle(title);
+  renderQuestionChoices(choices);
+}
+
+function renderQuestionTitle(question) {
+  let questionTitle = document.getElementById("question-title");
+  questionTitle.textContent = question;
+}
+
+function renderQuestionChoices(choices) {
+  let questionChoices = document.getElementById("choices");
+  questionChoices.innerHTML = "";
+
+  for (const choice of choices) {
+    let button = document.createElement("BUTTON");
+    button.textContent = choice;
+
+    button.addEventListener("click", onAnswerClick);
+
+    questionChoices.appendChild(button);
+  }
+}
+
+function onAnswerClick(e) {
+  renderQuestion(myQuestions[1].question, myQuestions[1].answers);
+
+  timeLeft -= 10;
+  timer.textContent = timeLeft;
+}
+
+// starts timer
+let timer = document.querySelector(".timer");
+let timeLeft = 75;
+
+function countdown() {
+  setInterval(function () {
+    timeLeft--;
+    timer.textContent = timeLeft;
+
+    if (timeLeft === 0) {
+      clearInterval(timerInterval);
+    }
+  }, 1000);
+}
 
 // timer starts
-    // Timer function
-    // countdown
-    // display
-    // get game stop
-    // stop at zero
+// Timer function
+// countdown
+// display
+// get game stop
+// stop at zero
 
 //
 
 // Timer connected to:
-    // Game start f
-    // Game stop f
-    // timer display
-    // Incorrect answer - time deduction
+// Game start f
+// Game stop f
+// timer display
+// Incorrect answer - time deduction
 
 // gamestart function
-    // trigger from button
-    // start timer
-    // call show questions and choices function
-    // hide submit button
-    // hide instructions
+// trigger from button
+// start timer
+// call show questions and choices function
+// hide submit button
+// hide instructions
 
 // toggle class attribute hide
-    // start-screen
-    // questions container
+// start-screen
+// questions container
 
 // Gamestop function
-    // trigger from
-        // last question
-        // timer zero
-    // announces score
-    // gets initials
-    // post to local storage
-    // show submit button
-    // show instructions
+// trigger from
+// last question
+// timer zero
+// announces score
+// gets initials
+// post to local storage
+// show submit button
+// show instructions
 
 // highscore function
-    // on load, update from local storage
-    // rank by score
-    // activate clear highscores button
+// on load, update from local storage
+// rank by score
+// activate clear highscores button
 
 // clear highscore function
-    // clears LS
+// clears LS
 // Q&A function
-    // get q&A
-    // display
-    // interactive
-    // on click
-        // show choice
-        // show correct answer
-        // clear
-        // get new questions
+// get q&A
+// display
+// interactive
+// on click
+// show choice
+// show correct answer
+// clear
+// get new questions
 
 // choice display
 
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,6 +1,14 @@
 var myQuestions = [
   {
     question: "Inside which HTML element do we put the JavaScript?",
-    answers: ["<script>", "<javascript>", "<js>"],
+    answers: ["1. <script>", "2. <javascript>", "3. <js>"],
+  },
+  {
+    question: "Where is the correct place to insert a JavaScript?",
+    answers: [
+      "1. The <head> section",
+      "2. The <body> section",
+      "3. Both the <head> section and the <body> section are correct",
+    ],
   },
 ];
